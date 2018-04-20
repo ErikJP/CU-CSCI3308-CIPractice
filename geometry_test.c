ecd4db1f8c2135a9d8067953bfdfc3e3a0a302c3
ErikJP <erik.pronk@colorado.edu>
2018-04-19 19:13:04 -0600

    Trying to run things

--- geometry_test.c
+++ geometry_test.c
@@ -154,7 +154,7 @@ START_TEST(test_2d_area_triangle){
     c.x = 1;
     c.y = 0;
     
-    ck_assert(coord_2d_area_triangle($a,$b,$c) == 0.5);
+    ck_assert(coord_2d_area_triangle(&a,&b,&c) == 0.5);
 
     a.x = 1
     a.y = 0;
@@ -162,7 +162,7 @@ START_TEST(test_2d_area_triangle){
     c.x = 1;
     c.y = 0;
 
-    ck_assert(coord_2d_area_triangle($a,$b,$c) == 0.5)
+    ck_assert(coord_2d_area_triangle(&a,&b,&c) == 0.5)
 
 }
 END_TEST
@@ -184,10 +184,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_area_triangle = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_area_triangle);
 
     /* Return Suite */
     return s;
